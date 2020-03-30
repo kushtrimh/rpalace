0580f11ca52984720cfcd18d349fc17140d3d127
kushtrimh <kushtrimmh13@gmail.com>
2020-03-30 21:02:26 +0200

    Initial commit

---
+++ src/main/java/org/kushtrimhajrizi/rpalace/RpalaceApplication.java
@@ -0,0 +1,13 @@
+package org.kushtrimhajrizi.rpalace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpalaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpalaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kushtrimhajrizi/rpalace/RpalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kushtrimhajrizi.rpalace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpalaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
