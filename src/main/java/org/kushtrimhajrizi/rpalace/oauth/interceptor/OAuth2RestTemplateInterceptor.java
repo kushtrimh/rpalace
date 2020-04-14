4efa03ec9c5164d9e0492c14de3b1e0619cbe74b
kushtrimh <kushtrimmh13@gmail.com>
2020-04-14 21:36:03 +0200

    Version upgrade and JDBC Auth service support added

--- src/main/java/org/kushtrimhajrizi/rpalace/oauth/interceptor/OAuth2RestTemplateInterceptor.java
+++ src/main/java/org/kushtrimhajrizi/rpalace/oauth/interceptor/OAuth2RestTemplateInterceptor.java
@@ -2,6 +2,7 @@ package org.kushtrimhajrizi.rpalace.oauth.interceptor;
 
 
 import org.kushtrimhajrizi.rpalace.oauth.OAuth2TokenService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
@@ -12,7 +13,11 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@Deprecated
 public class OAuth2RestTemplateInterceptor implements ClientHttpRequestInterceptor {
+
+    @Value("${rpalace.user-agent}")
+    private String userAgent;
 
     private final OAuth2TokenService oAuth2TokenService;
 
@@ -23,8 +28,10 @@ public class OAuth2RestTemplateInterceptor implements ClientHttpRequestIntercept
     @Override
     public ClientHttpResponse intercept(HttpRequest httpRequest, byte[] bytes,
             ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
-        oAuth2TokenService.getToken().ifPresent(token ->
-            httpRequest.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " + token));
+        oAuth2TokenService.getToken().ifPresent(token -> {
+            httpRequest.getHeaders().add(HttpHeaders.AUTHORIZATION, "bearer " + token);
+            httpRequest.getHeaders().add(HttpHeaders.USER_AGENT, userAgent);
+        });
         return clientHttpRequestExecution.execute(httpRequest, bytes);
     }
 }
--- src/main/java/org/kushtrimhajrizi/rpalace/security/config/SecurityConfig.java
+++ src/main/java/org/kushtrimhajrizi/rpalace/security/config/SecurityConfig.java
@@ -1,48 +1,26 @@
 package org.kushtrimhajrizi.rpalace.security.config;
 
 import org.kushtrimhajrizi.rpalace.security.PermissionType;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
-import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationExchange;
-import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.List;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Value("${rpalace.user-agent}")
-    private String userAgent;
-
-    private final ClientRegistrationRepository clientRegistrationRepository;
     private final DefaultUserDetailsService defaultUserDetailsService;
+    private final OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> oAuth2AccessTokenResponseClient;
 
-    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository,
-                          DefaultUserDetailsService defaultUserDetailsService) {
-        this.clientRegistrationRepository = clientRegistrationRepository;
+    public SecurityConfig(DefaultUserDetailsService defaultUserDetailsService,
+                          OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> oAuth2AccessTokenResponseClient) {
         this.defaultUserDetailsService = defaultUserDetailsService;
+        this.oAuth2AccessTokenResponseClient = oAuth2AccessTokenResponseClient;
     }
 
     @Override
@@ -59,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin(login -> login.successForwardUrl("/oauth"))
             .oauth2Client(oauth2Client -> oauth2Client
                 .authorizationCodeGrant(codeGrant ->
-                        codeGrant.accessTokenResponseClient(oAuth2AccessTokenResponseClient())));
+                        codeGrant.accessTokenResponseClient(oAuth2AccessTokenResponseClient)));
     }
 
     @Bean
@@ -73,42 +51,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setUserDetailsService(defaultUserDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
-    }
-
-    @Bean
-    public ClientRegistration redditClientRegistration() {
-        return clientRegistrationRepository.findByRegistrationId("reddit");
-    }
-
-    @Bean
-    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> oAuth2AccessTokenResponseClient() {
-        DefaultAuthorizationCodeTokenResponseClient defaultAuthorizationClient =
-                new DefaultAuthorizationCodeTokenResponseClient();
-        ClientRegistration clientRegistration = redditClientRegistration();
-
-        String credentials = clientRegistration.getClientId() + ":" + clientRegistration.getClientSecret();
-        String basicAuthorizationCode = Base64.getEncoder().encodeToString(
-                credentials.getBytes(StandardCharsets.UTF_8));
-        defaultAuthorizationClient.setRequestEntityConverter((authorizationCodeGrantRequest) -> {
-            OAuth2AuthorizationExchange authorizationExchange = authorizationCodeGrantRequest.getAuthorizationExchange();
-            MultiValueMap<String, String> formParameters = new LinkedMultiValueMap<>();
-            formParameters.add("grant_type", clientRegistration.getAuthorizationGrantType().getValue());
-            formParameters.add("code", authorizationExchange.getAuthorizationResponse().getCode());
-            formParameters.add("redirect_uri", clientRegistration.getRedirectUriTemplate());
-            return RequestEntity
-                    .post(URI.create(clientRegistration.getProviderDetails().getTokenUri()))
-                    .header(HttpHeaders.AUTHORIZATION, "Basic " + basicAuthorizationCode)
-                    .header(HttpHeaders.USER_AGENT, userAgent)
-                    .body(formParameters);
-        });
-
-        RestTemplate restTemplate = new RestTemplate(List.of(
-                new FormHttpMessageConverter(),
-                new OAuth2AccessTokenResponseHttpMessageConverter())
-        );
-        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
-        defaultAuthorizationClient.setRestOperations(restTemplate);
-
-        return defaultAuthorizationClient;
     }
 }
